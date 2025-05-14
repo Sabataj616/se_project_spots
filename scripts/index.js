ec8ded265ef40592db6fc65de03e7ed8e30ce6ce
Tajgi Fields <Tajgi@icloud.com>
2025-05-14 14:13:15 -0400

    P4 Stage3 2nd revisions

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const modalClosebtn = editProfileModal.querySelector(".modal__close-btn");
-const addProfileBtn = document.querySelector(".profile__add-btn");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
@@ -13,7 +13,7 @@ const editProfileNameInput = editProfileModal.querySelector(
 );
 const editProfileDescriptionInput =
   editProfileModal.querySelector("#description-input");
-const profileFormElement = document.querySelector(".modal__form");
+const profileFormElement = editProfileModal.querySelector(".modal__form");
 
 const cardFormElement = newPostModal.querySelector(".modal__form");
 const imageLinkInput = cardFormElement.querySelector("#image-link-input");
@@ -22,8 +22,6 @@ const captionInput = cardFormElement.querySelector("#caption-input");
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  profileNameEl.textContent = "Bessie Coleman";
-  profileDescriptionEl.textContent = "Civil Aviator";
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -31,10 +29,8 @@ modalClosebtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-addProfileBtn.addEventListener("click", function () {
+newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
-  imageLinkInput.value = someThing.textContent;
-  captionInput.value = someOtherThing.textContent;
 });
 
 newPostCloseBtn.addEventListener("click", function () {
@@ -43,7 +39,6 @@ newPostCloseBtn.addEventListener("click", function () {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  console.log("submitting");
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
   editProfileModal.classList.remove("modal_is-opened");
@@ -53,10 +48,6 @@ profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
-  console.log("submitting");
-  someThing.textContent = imageLinkInput.value;
-  someOtherThing.textContent = captionInput.value;
-
   newPostModal.classList.remove("modal_is-opened");
 }
 
